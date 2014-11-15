0f38e29ba2d95eb54268930f8d4618fe654a04ae
Adnan_Uddin <adnan.uddin6@gmail.com>
2014-11-15 16:08:21 -0500

    implemented picture normalization (not too sure if results are good)

---
+++ normalize.py
@@ -0,0 +1,48 @@
+from PIL import Image
+import glob, os, math
+
+# test image matrix
+s = Image.open("./training dataset/9_9_.gif")
+um = 100
+ustd = 80
+(width, height) = s.size
+# print s.size 
+# print height
+img_data = list(s.getdata())
+# compare all pixels in row
+for j in xrange(0, height -1 ):
+	for i in xrange(0, width -1):
+		if (img_data[j*width + i] != s.getpixel((i,j)) ):
+			print "not equal"
+			break;
+
+# #compare img_data with get pixels
+# for i in xrange(0, width*height - 1)
+	
+for i in xrange(0,height - 1):
+	# mean of ith column
+	mean = 100.0;
+	# mean1 = numpy.mean(img_data[i:height-1:width]);
+	for j in xrange(0,width- 1):
+		mean += s.getpixel((j,i))
+	mean /= width 
+	# print mean1 - mean2
+	# get std
+	std = 0.0
+	for j in xrange(0,width -1):
+		std = math.pow(s.getpixel((j,i)) - mean, 2)
+
+	std = math.sqrt(std/(width-1))
+	for j in xrange(0,width -1):
+		npixel = (s.getpixel((j,i)) - mean)*ustd/std + um
+		s.putpixel((j,i), npixel)		
+
+
+s.save("normalized.gif")
+	#standard deviation
+
+	# print mean
+
+
+	#do work
+
